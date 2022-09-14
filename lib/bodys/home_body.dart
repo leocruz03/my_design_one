5af7aca3fc522da99523d103d74480af232af001
Leonado De la cruz <leonardodelacruz03@hotmail.com>
2022-09-14 10:04:02 -0500

    start design in home body :art:

--- lib/bodys/home_body.dart
+++ lib/bodys/home_body.dart
@@ -1,14 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeBody extends StatelessWidget {
   const HomeBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home'),
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 10,
+            vertical: 20,
+          ),
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    child: const _UserBodyHome(),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Column(
+                    children: const [
+                      Text('APL Costos'),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class _UserBodyHome extends StatelessWidget {
+  const _UserBodyHome({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'Bienvenid@',
+          style: GoogleFonts.raleway(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        const SizedBox(height: 20),
+        Text(
+          'username worker',
+          style: GoogleFonts.raleway(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
     );
   }
 }
